b48410dcbaba861449029d13f082de4dbded430d
Tserewara <alvaro.alf2@gmail.com>
2021-03-18 18:08:45 -0300

    Update start_db.py

--- Dockerfile
+++ Dockerfile
@@ -3,3 +3,4 @@ FROM postgres:13.2-alpine
 EXPOSE 5432
 
 COPY init.sql /docker-entrypoint-initdb.d/
+
--- start_db.py
+++ start_db.py
@@ -1,6 +1,6 @@
 import psycopg2
 try:
-    conn = psycopg2.connect("host=localhost dbname=postgres")
+    conn = psycopg2.connect("host=localhost dbname=blog")
     conn.close()
 except psycopg2.OperationalError as ex:
     print("Connection failed: {0}".format(ex))
